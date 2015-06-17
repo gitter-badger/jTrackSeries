06966c07af686ba83d9b9be0b6614ee78c23a4dc
CristinaSilvaEng <cristina.silva@openmailbox.org>
2015-06-17 05:25:19 -0300

    Initial Commit

---
+++ src/main/java/servlet/HelloServlet.java
@@ -0,0 +1,29 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Cristina on 17/06/2015.
+ */
+
+@WebServlet(
+        name = "HelloServlet",
+        urlPatterns = {"/HelloWorld"}
+)
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter writer = response.getWriter();
+        writer.append("<html><head><title>Hello World</title></head><body><p>Hello, World!</p></body></html>");
+        writer.close();
+    }
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Bye, World!</title>
+</head>
+<body>
+    <p>Bye, World!</p>
+</body>
+</html>
